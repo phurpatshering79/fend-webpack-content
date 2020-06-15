ca9027a8814fb0ba5581fc0b840521f024f46b08
phurpatshering79 <phurpa.tshering79>
2020-06-15 18:35:32 -0400

    Seperate the dev and prod webpack config files and create scripts for both

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',       //RegExp to locate the files that our loader runs on
+            exclude: /node_modules/,    //File that beeds to be excluded, loader does not run here
+            loader: "babel-loader"  //the loader that we are using OR plugins || for our webpack
+                }
+                ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
