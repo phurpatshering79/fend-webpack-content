731de0956114927dd2a919d1818db9b95eb1046c
phurpatshering79 <phurpa.tshering79>
2020-06-15 17:10:40 -0400

    Integrate Babel to webpack along with configuration for both

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',       //RegExp to locate the files that our loader runs on
+            exclude: /node_modules/,    //File that beeds to be excluded, loader does not run here
+            loader: "babel-loader"  //the loader that we are using OR plugins || for our webpack
+                }
+        ]
+}
 }
