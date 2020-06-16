2da1a23f72703b7eb780e59c2ad4782f3904d6ab
phurpatshering79 <phurpa.tshering79>
2020-06-16 13:39:50 -0400

    add webpack-server and additional plugins

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +20,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets:false,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin({
+            generateStatsFile: true
         })
     ]
 }
